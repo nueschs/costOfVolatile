d2fd20c2320bb9960e50d4eab1ec9553dbe74d8e
Stefan Nüesch <snuesch@gmx.ch>
2013-04-23 09:25:18 +0200

    Some refinements to Mapping

--- SSC/src/ch/unibnf/ssc/vol/Mapping.java
+++ SSC/src/ch/unibnf/ssc/vol/Mapping.java
@@ -4,19 +4,20 @@ import java.util.Random;
 
 public class Mapping {
 
-	public static final String[] KEYS = Config.KEYS_1;
-	public static final int NUM_PRODUCERS = 8;
-	public static final int NUM_CONSUMERS = 8;
-	public static final int NUM_WRITES = 8;
-	public static final int NUM_READS = 100;
-
 	/**
 	 * @param args
+	 *            args[0] = KeySet to Use, args[1] = Number of producer threads,
+	 *            args[2] = number of consumer threads, args[3] = number of
+	 *            write cycles for each writer thread, args[4] = number of read
+	 *            cycles
+	 * 
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		Mapping mapping = new Mapping();
 
+		final Mapping mapping = new Mapping(args);
+		System.out.println("Running with parameters " + args[0] + " " + args[1]
+				+ " " + args[2] + " " + args[3] + " " + args[4]);
 		System.out.println(">>> Warm-up pass");
 		for (int i = 0; i < 10; i++) {
 			mapping.execute();
@@ -30,53 +31,102 @@ public class Mapping {
 		System.out.println("<<< Run benchmark");
 	}
 
-	private final Pair[] pairs = { new Pair(KEYS[0]), new Pair(KEYS[1]),
-			new Pair(KEYS[2]), new Pair(KEYS[3]), new Pair(KEYS[4]),
-			new Pair(KEYS[5]), new Pair(KEYS[6]), new Pair(KEYS[7]),
-			new Pair(KEYS[8]), new Pair(KEYS[9]) };;
+	public final String[] KEYS;
+	public final int NUM_PRODUCERS;
+	public final int NUM_CONSUMERS;
+	public final int NUM_WRITES;
+
+	public final int NUM_READS;
+
+	private final Pair[] pairs;
+
+	public Mapping(String[] args) {
+		int keys = Integer.parseInt(args[0]);
+		if (keys > 1 || keys < 0) {
+			throw new IllegalArgumentException(
+					"Keys supports values 0 and 1 only");
+		}
+
+		this.KEYS = keys == 0 ? Config.KEYS_1 : Config.KEYS_64;
+
+		this.NUM_PRODUCERS = Integer.parseInt(args[1]);
+		this.NUM_CONSUMERS = Integer.parseInt(args[2]);
+		this.NUM_WRITES = Integer.parseInt(args[3]);
+		this.NUM_READS = Integer.parseInt(args[4]);
+
+		this.pairs = new Pair[this.KEYS.length];
+
+		for (int i = 0; i < this.KEYS.length; i++) {
+			this.pairs[i] = new Pair(this.KEYS[i]);
+		}
+	};
 
 	private void execute() throws InterruptedException {
 
-		final Thread[] threads = new Thread[NUM_CONSUMERS + NUM_PRODUCERS];
-		// starting producer threads
-		long start = System.nanoTime();
+		final Thread[] threads = new Thread[this.NUM_CONSUMERS
+				+ this.NUM_PRODUCERS];
+		final int len = this.KEYS.length / this.NUM_PRODUCERS;
 
-		final int len = KEYS.length / NUM_PRODUCERS;
-		for (int i = 0; i < NUM_PRODUCERS; i++) {
+		// starting producer threads
+		for (int i = 0; i < this.NUM_PRODUCERS; i++) {
 			final int localCount = i;
 			threads[i] = new Thread() {
 
+				int[] writePositions = this.getWritePositions();
+
 				Random r = new Random();
-				int start = (len * localCount);
 
 				@Override
 				public void run() {
-					for (int j = 0; j < len; j++) {
-						for (int k = 0; k < NUM_WRITES; k++) {
-							Mapping.this.pairs[len + this.start].setData(this.r
-									.nextLong());
+					for (int k = 0; k < Mapping.this.NUM_WRITES; k++) {
+						for (int j = 0; j < this.writePositions.length; j++) {
+							Mapping.this.pairs[this.writePositions[j]]
+									.setData(this.r.nextLong());
 						}
 					}
 				}
 
+				/*
+				 * Distribute keys evenly to Threads (e.g. 10 Keys, 4 Threads:
+				 * {0,4,8},{1,5,9},{2,6},{3,7})
+				 */
+				private int[] getWritePositions() {
+					int additional = localCount < (Mapping.this.KEYS.length % Mapping.this.NUM_PRODUCERS) ? 1
+							: 0;
+					int[] writePositions = new int[len + additional];
+					int count = 0;
+					for (int i = 0; i < Mapping.this.KEYS.length; i++) {
+						if (localCount == i % Mapping.this.NUM_PRODUCERS) {
+							writePositions[count] = i;
+							count++;
+						}
+					}
+					return writePositions;
+				}
+
 			};
-			threads[i].start();
 		}
 
 		// starting consumer threads
-		for (int i = 0; i < NUM_CONSUMERS; i++) {
-			final int localCount = i;
-			threads[i + NUM_PRODUCERS] = new Thread() {
+		for (int i = 0; i < this.NUM_CONSUMERS; i++) {
+			final Random r = new Random();
+			threads[i + this.NUM_PRODUCERS] = new Thread() {
 				@Override
 				public void run() {
-					for (int j = 0; j < NUM_READS; j++) {
-						long temp = Mapping.this.pairs[KEYS.length
-								/ NUM_CONSUMERS + localCount].getData();
+					for (int j = 0; j < Mapping.this.NUM_READS; j++) {
+						long temp = Mapping.this.pairs[r
+								.nextInt(Mapping.this.KEYS.length)].getData();
 					}
 				}
 
 			};
-			threads[i + NUM_PRODUCERS].start();
+		}
+
+		// starttime measured after Thread creation
+		long start = System.nanoTime();
+
+		for (Thread thread : threads) {
+			thread.start();
 		}
 
 		for (Thread thread : threads) {
@@ -84,10 +134,6 @@ public class Mapping {
 		}
 		long end = System.nanoTime();
 		System.out.println(end - start);
-
-		for (Pair pair : this.pairs) {
-			System.out.println(pair);
-		}
 	}
 
 	public class Pair {
